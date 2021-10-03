78bb5d616bd957790c82181fcd0ebe88c441b1a8
Jamswhat2 <hilarionjames25@gmail.com>
2021-10-03 19:38:37 -0400

    implemented objectkeys function

--- Array.prototypes.js
+++ Array.prototypes.js
@@ -397,7 +397,45 @@ Array.prototype.lastIndexOf_ = function(searchElement, startIndex) {
  console.log("lastIndexOf (expected):", lastIndexOfarray1.lastIndexOf(2, -1)) 
 */
 
+/* Object.keys() */
+/**
+ * The Object.keys() method returns an array of a given object's own enumerable property names, 
+ * iterated in the same order that a normal loop would.
+ */
+Object.keys_ = function(obj) {
+    let keys = []
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            keys.push(key)
+        }
+    }
+    return keys
+}
+// Test for Object.keys
+/*
+ const object1 = {
+     a: 'somestring',
+     b: 42,
+     c: false
+ };
+ console.log(Object.keys(object1)); // expected output: Array ["a", "b", "c"]
+ console.log(Object.keys_(object1));
+ // simple array
+ const arr = ['a', 'b', 'c'];
+ console.log(Object.keys(arr));     // console: ['0', '1', '2']
+ console.log(Object.keys_(arr));
 
-// Object.keys()
-
+ console.log("")
+ // array-like object
+ const obj = { 0: 'a', 1: 'b', 2: 'c' };
+ console.log(Object.keys(obj));     // console: ['0', '1', '2']
+ console.log(Object.keys_(obj));
+ // array-like object with random key ordering
+ const anObj = { 100: 'a', 2: 'b', 7: 'c' };
+ console.log(Object.keys(anObj));   // console: ['2', '7', '100']
+ console.log(Object.keys_(anObj));
+ console.log(Object.keys({}));      // []
+ console.log(Object.keys_({})); 
+*/
+ 
 // Object.values()
